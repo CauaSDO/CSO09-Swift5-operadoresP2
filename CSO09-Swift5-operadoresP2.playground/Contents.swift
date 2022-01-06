47c2bd4775bac91b3c407ea20ffcf2d65d9ad6f4
CauaSDO <cauasilva@icloud.com>
2022-01-06 18:34:07 -0300

    OPERDORES P2 CONCLUÍDO

--- CSO09-Swift5-operadoresP2.playground/Contents.swift
+++ CSO09-Swift5-operadoresP2.playground/Contents.swift
@@ -18,4 +18,22 @@ false && true
 var likesMeat = false
 var likesBeer = true
 var canInviteBarbecue = likesBeer || likesMeat
+true || true
+true || false
+false || false
+false || true
 
+var grade1 = 8.5
+var grade2 = 7.0
+let result = (grade1+grade2)/2
+
+//Operador NOT (!)
+var knowSwift = false
+var enrolSwiftCourse = !knowSwift
+
+//Operador ternário
+var grade = 7.75
+var gradeResultado = (grade >= 7.0) ? "Aprovado" : "Reprovado"
+
+let number = 10
+let type = (number % 2 == 0) ? "par" : "Ímpar"

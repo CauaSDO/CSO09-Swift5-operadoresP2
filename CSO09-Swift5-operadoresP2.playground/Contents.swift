12709b4e7b7e67ac0856289cd70eba302eb5ba07
CauaSDO <cauasilva@icloud.com>
2022-01-06 18:09:27 -0300

    Initial Commit

---
+++ CSO09-Swift5-operadoresP2.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//OPERADORES PARTE 2
+
+//Operador AND (&&)
+var boughtBanana: Bool = true
+var boughtTomato: Bool = true
+var boughtApple: Bool = false
+var isHappy = boughtBanana && boughtTomato && boughtApple
+true && true
+true && false
+false && false
+false && true
+
+//Operador OR (||)
+var likesMeat = false
+var likesBeer = true
+var canInviteBarbecue = likesBeer || likesMeat
+
